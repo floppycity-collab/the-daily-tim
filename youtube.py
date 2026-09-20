9b1c9c409f58a95ddad56d90725276abb904df18
floppycity-collab <floppycity@gmail.com>
2026-09-20 15:51:49 +0200

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -27,7 +27,7 @@ async def create_webhook(title, link, youtube_name, date):
         ## await send(content=..., *, username=..., avatar_url=..., tts=False, ephemeral=..., suppress_embeds=..., flags=..., file=..., files=..., embed=..., embeds=..., allowed_mentions=..., view=..., components=..., thread=..., thread_name=..., applied_tags=..., wait=False, delete_after=..., poll=...)
 
         webhook_username = "The Daily Tim" # enter name of the Webhook user, can be anything (in this case, it's YouTube Bot)
-        webhook_avatar = "https://i.imgur.com/UtSQiYm.png" # enter a url to a pic if you want a avatar
+        webhook_avatar = "https://i.imgur.com/JiNRhdU.png" # enter a url to a pic if you want a avatar
         await webhook.send(content=f"NEW UPLOAD FROM {youtube_name}!\n'{title}'\n{link}\n{date}", username=webhook_username, avatar_url=webhook_avatar)
         await asyncio.sleep(3)
 
